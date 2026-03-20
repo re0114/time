db86b17f3a938bfa1c055807c3896774578ae381
re0114 <dengciaocheng@163.com>
2026-03-20 18:52:50 +0800

    ex03-1: 初始版本 - 使用millis实现1Hz闪烁

---
+++ ex03/ex03_1Hz_blink/ex03_1Hz_blink.ino
@@ -0,0 +1,20 @@
+// 实验3任务1：使用millis()实现1Hz LED闪烁
+const int ledPin = 2;
+unsigned long previousMillis = 0;
+const long interval = 1000;   // 1Hz
+int ledState = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+    Serial.println(ledState ? "ON" : "OFF");
+  }
+}
